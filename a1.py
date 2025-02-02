6e9e671e319ad65e670ba524b215d56b74320ef5
dipalitayde <dipalitayde2009@gmail.com>
2025-02-02 17:50:08 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,15 @@
+
+temprature= int(input("enter the temprature"))
+
+if temprature<=20 :
+ print("winter days")
+
+elif temprature>=25 :
+   
+   print("mid winter days")
+
+elif temprature>=30 :
+  print("hotty days")
+
+
+
